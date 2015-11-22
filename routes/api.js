bdc6d22972480acc962f246e28638b5a66e976f3
olivif <olly2204@yahoo.com>
2015-11-22 21:19:01 +0100

    Adding coverage numbers to travis.ci. Cleaning up unused routes. Adding simple api tests.

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var crawler = require('./../lib/crawler');
+var express = require("express");
+var crawler = require("./../lib/crawler");
 var router = express.Router();
 
 /* GET api heartbeat. */
--- test/test.js
+++ test/test.js
@@ -1,12 +1,13 @@
-var should = require('should');
-var assert = require('assert'); 
-var request = require('supertest');  
+var should = require("should");
+var assert = require("assert"); 
+var request = require("supertest");  
+var main = require("./../main");  
 
 // Test suite for testing functionality
-describe('testing', function () {
+describe("testing", function () {
   
   // testing the test framework
-  it('should be able to assert 1+1', function (done) {
+  it("should be able to assert 1+1", function (done) {
     var two = 1 + 1;
     two.should.equal(2);
     done();
@@ -15,11 +16,45 @@ describe('testing', function () {
 });
 
 // Test suite for crawler functionality
-describe('crawler', function () {
+describe("crawler", function () {
   
 });
 
 // Test suite for api routing
-describe('api', function () {
+describe("api", function () {
+
+  var app;
+
+  before(function() {
+    // runs before all tests in this block
+    app = main.setupApp();
+  });
+
+  after(function() {
+    // runs after all tests in this block
+  });
+  
+  beforeEach(function() {
+    // runs before each test in this block
+  });
+
+  afterEach(function() {
+    // runs after each test in this block
+  });
+  
+  it("should have an index", function (done) {
+
+    request(app)
+      .get("/")
+      .set('Accept', 'application/json') 
+      .expect(200, done); 
+  });
+
+  it("should have /api", function (done) {
+
+    request(app)
+      .get("/api")
+      .expect(200, done); 
+  });
   
 });
