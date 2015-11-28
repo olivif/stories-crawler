6f518d9e87677c909fca67fe45f4a5df480b7219
olivif <olly2204@yahoo.com>
2015-11-28 17:00:30 +0100

    Adding jscs lint. Fixing code style issues.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
+var gulp = require("gulp");
+var mocha = require("gulp-mocha");
 
 /**
  * Execute all tests.
  */
-gulp.task('run-tests', function () {
-  return gulp.src('test/**/*.js', { read: false })
-    .pipe(mocha({ reporter: 'spec' }));
+gulp.task("run-tests", function() {
+  return gulp.src("test/**/*.js", {read: false})
+    .pipe(mocha({reporter: "spec"}));
 });
--- routes/api.js
+++ routes/api.js
@@ -3,23 +3,23 @@ var storyProvider = require("./../lib/storyProvider");
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function(request, response, next){
-	response.send("Api is running.");
+router.get("/", function(request, response, next) {
+  response.send("Api is running.");
 });
 
-router.get("/test", function(request, response, next){
-	throw "exception";
+router.get("/test", function(request, response, next) {
+  throw "exception";
 });
 
-router.get("/stories", function(request, response, next){
-	
-	// get stories from crawler
-	var storiesCallback = function(stories) {
-		var data = {stories: stories};
-		response.send(data);
-	};
-	
-	storyProvider.getStories(storiesCallback);
+router.get("/stories", function(request, response, next) {
+
+  // get stories from crawler
+  var storiesCallback = function(stories) {
+    var data = {stories: stories};
+    response.send(data);
+  };
+
+  storyProvider.getStories(storiesCallback);
 });
 
 module.exports = router;
