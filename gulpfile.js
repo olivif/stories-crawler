5a1a7ec4984bcf4429f8ad427e70a85d5026378d
olivif <olly2204@yahoo.com>
2015-12-12 13:10:51 +0100

    Adding gulp task for local dev. Fixes #5

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
-"use strict";
-
 var gulp = require("gulp");
 var mocha = require("gulp-mocha");
+var nodemon = require('gulp-nodemon');
 
 /**
  * Execute all tests.
@@ -9,4 +8,21 @@ var mocha = require("gulp-mocha");
 gulp.task("run-tests", function() {
   return gulp.src("test/**/*.js", {read: false})
     .pipe(mocha({reporter: "spec"}));
+});
+
+/**
+ * Run the API in a loop, restart when files change
+ */
+gulp.task('default', function(){
+    nodemon({
+        script: './bin/www',
+        ext: 'js',
+        env: {
+            PORT:8000
+        },
+        ignore: ['./node_modules/**']
+    })
+    .on('restart', function(){
+        console.log('Restarting');
+    });
 });
