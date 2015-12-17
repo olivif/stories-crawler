35b9a9566cf62845eb8bdd7cbeb4e180ed1a560d
olivif <olly2204@yahoo.com>
2015-12-17 16:01:00 +0100

    Revert "Adding ApiDoc style comments to index and api."
    This reverts commit 22b16be8545fc301fcb449735bbe042c9cad82cd.

--- routes/api.js
+++ routes/api.js
@@ -7,35 +7,14 @@ var storyProvider = require("./../lib/storyProvider");
 
 var storyController = require("./../lib/controllers/storyController")(Story, Event, storyProvider);
 
-/**
- * @api {get} /api 
- * @apiName apiIndex
- * @apiGroup api
- *
- * @apiSuccess {String} Api is running.
- */
 router.get("/", function(request, response, next) {
   response.send("Api is running.");
 });
 
-/**
- * @api {get} /api/test 
- * @apiName apiTest
- * @apiGroup api
- *
- * @apiError UnexpectedException
- */
 router.get("/test", function(request, response, next) {
-  throw "UnexpectedException";
+  throw "exception";
 });
 
-/**
- * @api {get} /api/stories 
- * @apiName apiStories
- * @apiGroup api
- *
- * @apiSuccess {String} Api is running.
- */
 router.get("/stories", function(request, response, next) {
   storyController.get(request, response, null);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-/**
- * @api {get} / Index
- *
- * @apiSuccess []
- */
+/* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", {title: "Express"});
 });
